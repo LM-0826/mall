20d7eb1f01752a278ef9bcaa6596733cfa947dfa
liming <317174545@qq.com>
2019-11-08 16:37:43 +0800

    redis和切面以及实体

--- src/main/java/com/kidsphoto/mall/entity/Good.java
+++ src/main/java/com/kidsphoto/mall/entity/Good.java
@@ -1,8 +1,10 @@
 package com.kidsphoto.mall.entity;
 
+import java.io.Serializable;
+
 /**
  * @author 李明
  * @create 2019-11-08 15:06
  */
-public class Good extends D {
+public class Good extends Data implements Serializable {
 }
