ff38881e23a91270750b01e4fa2e9dfe12c7d6f5
liming <317174545@qq.com>
2019-11-08 16:37:33 +0800

    redis和切面以及实体

--- src/main/java/com/kidsphoto/mall/controller/LoginController.java
+++ src/main/java/com/kidsphoto/mall/controller/LoginController.java
@@ -1,8 +1,12 @@
 package com.kidsphoto.mall.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author 李明
  * @create 2019-11-07 10:36
  */
+@RestController
 public class LoginController {
 }
